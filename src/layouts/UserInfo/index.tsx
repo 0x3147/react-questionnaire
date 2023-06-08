f15a9c8ed068d02d33d8ea003681784c326d4e1d
Cooper <radeonk@foxmail.com>
2023-06-08 16:46:14 +0800

    feat: 用户注册、登录以及获取token

--- src/layouts/UserInfo/index.tsx
+++ src/layouts/UserInfo/index.tsx
@@ -1,7 +1,11 @@
 import React, { memo } from 'react'
-import { Link } from 'react-router-dom'
-import { Space } from 'antd'
-import { LOGIN_PATH, REGISTER_PATH } from '@/router'
+import { Link, useNavigate } from 'react-router-dom'
+import { Button, message } from 'antd'
+import { LOGIN_PATH } from '@/router'
+import { useRequest } from 'ahooks'
+import { getUserInfoService } from '@/services/user'
+import { UserOutlined } from '@ant-design/icons'
+import { removeToken } from '@/utils/user-token'
 
 import type { FC, ReactNode } from 'react'
 
@@ -9,15 +13,42 @@ interface IProps {
   children?: ReactNode
 }
 
+/**
+ * @desc 用户信息组件
+ * @Author bk0x114
+ * @Date 2023-06-08 16:39:43
+ */
 const UserInfo: FC<IProps> = () => {
-  return (
-    <div>
-      <Space>
-        <Link to={LOGIN_PATH}>登录</Link>
-        <Link to={REGISTER_PATH}>注册</Link>
-      </Space>
-    </div>
+  const { data } = useRequest(getUserInfoService)
+  const { username, nickname } = data || {}
+  const nav = useNavigate()
+
+  /**
+   * @desc 退出登录
+   * @Author bk0x114
+   * @Date 2023-06-08 16:40:23
+   */
+  const logout = () => {
+    removeToken()
+    message.success('您已成功退出登录!')
+    nav(LOGIN_PATH)
+  }
+
+  const UserInfoView = (
+    <>
+      <span style={{ color: '#e8e8e8' }}>
+        <UserOutlined rev={undefined} />
+        {nickname}
+      </span>
+      <Button type="link" onClick={logout}>
+        退出登录
+      </Button>
+    </>
   )
+
+  const loginView = <Link to={LOGIN_PATH}>登录</Link>
+
+  return <div>{username ? UserInfoView : loginView}</div>
 }
 
 export default memo(UserInfo)
